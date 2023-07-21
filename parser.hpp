7dcda99d945bc3346fe30c1814427af96219f8c2
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-07-20 19:47:32 -0500

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -58,7 +58,8 @@ private:
         switch(yparser->ytoken->id)
         {
             case YamlTokens::user_def: {
-                puint8 user_defined_var = yparser->ytoken->token_value;
+                puint8 user_defined_var = new uint8[strlen((cpint8) yparser->ytoken->token_value)];
+                memcpy(user_defined_var, yparser->ytoken->token_value, strlen((cpint8) yparser->ytoken->token_value));
                 
                 get_token_from_lexer();
                 get_token_from_lexer();
@@ -67,24 +68,143 @@ private:
                 {
                     case YamlTokens::hex: {
                         ydata->add_yaml_data(user_defined_var, (puint16) yparser->ytoken->token_value, data_types::Hex);
-                        delete user_defined_var;
+                
                         break;
                     }
                     case YamlTokens::number: {
+                        ydata->add_yaml_data(user_defined_var, (puint16) yparser->ytoken->token_value, data_types::Dec);
+                        
                         break;
                     }
                     case YamlTokens::string: {
+                        ydata->add_yaml_data(user_defined_var, (puint16) yparser->ytoken->token_value, data_types::Str);
                         break;
                     }
                     case YamlTokens::character: {
+                        ydata->add_yaml_data(user_defined_var, (puint16) yparser->ytoken->token_value, data_types::Chr);
+                        
                         break;
                     }
                     default: break;
                 }
+
+                delete user_defined_var;
+                user_defined_var = nullptr;
+                break;
+            }
+            case YamlTokens::YamlEOF: {
+                goto end;
                 break;
             }
             default: break;
         }
+
+        get_token_from_lexer();
+        goto reloop;
+        
+        end:
+        return;
+    }
+
+    void evaluate_yaml_data()
+    {
+        yaml_file_data = all_yaml_data;
+
+        while(yaml_file_data->next != nullptr)
+        {
+            printf("%s\n", yaml_file_data->user_defined);
+            if(yaml_file_data->yod_test((cpint8) "os_type", true))
+            {
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "bit32") == 0)
+                    yod.type = 0x02; // 32-bit OS
+                else
+                    yod.type = 0x03; // 64-bit OS
+                
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "os_name", true))
+            {
+                yod.OS_name = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "os_version", true))
+            {
+                yod.OS_version = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "pref_FS", true))
+            {
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "FAMP") == 0)
+                { yod.FS_type = (uint8) 1 << 0x04; goto cont; }
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "FAT32") == 0)
+                { yod.FS_type = (uint8) 1 << 0x05; goto cont; }
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "ISO9660") == 0)
+                { yod.FS_type = (uint8) 1 << 0x06; goto cont; }
+
+                yod.FS_type = (uint8) 1 << 0x07;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "production", true))
+            {
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "yes") == 0)
+                { yod.in_production = true; goto cont; }
+
+                yod.in_production = false;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "disk_name", true))
+            {
+                yod.disk_name = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "auto_format", true))
+            {
+                if(strcmp((cpint8) yaml_file_data->vdata_as_string, "yes") == 0)
+                { yod.auto_format = true; goto cont; }
+
+                yod.auto_format = false; goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "bin_folder", true))
+            {
+                yod.bin_folder = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "kernel_source", true))
+            {
+                yod.kernel_source_filename = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "kernel_bin", true))
+            {
+                yod.kernel_bin_filename = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            if(yaml_file_data->yod_test((cpint8) "kernel_o", true))
+            {
+                yod.kernel_o_filename = yaml_file_data->vdata_as_string;
+                goto cont;
+            }
+
+            cont:
+            _next
+        }
+
+        printf("\n\nYAML OS Data:\n\tOS Type: %X (%s)\n\tOS In Production: %s\n\t",
+            yod.type, yod.type == 0x02 ? "bit32" : "bit64",
+            yod.in_production ? "Yes" : "No");
+        printf("OS Name: %s\n\tOS Version: %s\n\tFS Type: %X (%s)\n\t",
+            yod.OS_name, yod.OS_version,
+            yod.FS_type, yod.get_fs_name());
     }
 
 public:
@@ -96,6 +216,9 @@ public:
         /* Get the first token. */
         get_token_from_lexer();
         parse_yaml();
+
+        /* Evaluate the data. */
+        evaluate_yaml_data();
     }
 
     template<typename T>
