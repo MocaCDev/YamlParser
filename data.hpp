8d07e4e70b11eabcebfca22da77825a95a98ba21
Moca <36897041+MocaCDev@users.noreply.github.com>
2023-07-20 19:48:26 -0500

    Update data.hpp

--- data.hpp
+++ data.hpp
@@ -7,7 +7,7 @@ namespace YamlData
 struct yaml_os_data
 {
     uint8        type;
-    bool         has_second_stage;
+    bool         in_production;
 
     /* General OS information. */
     puint8       OS_name;
@@ -24,10 +24,25 @@ struct yaml_os_data
     puint8       bin_folder;
 
     /* Kernel info. */
-    puint8       kernel_filename;
+    puint8       kernel_source_filename;
     puint8       kernel_bin_filename;
-    puint8       kernel_bin_o_filename;
+    puint8       kernel_o_filename;
+
+    puint8 get_fs_name()
+    {
+        switch(FS_type)
+        {
+            case 1 << 0x04: return (puint8) "FAMP";break;
+            case 1 << 0x05: return (puint8) "FAT32";break;
+            case 1 << 0x06: return (puint8) "ISO9660";break;
+            case 1 << 0x07: return (puint8) "EXT";break;
+            default: break;
+        }
+        return (puint8) "Unknown FS";
+    }
 };
+
+static struct yaml_os_data yod;
 
 enum class data_types
 {
@@ -42,6 +57,7 @@ struct data
     puint8        user_defined;
     data_types    data_type;
     puint16       vdata;
+    puint8        vdata_as_string;
 
     struct data   *next;
     struct data   *previous;
@@ -52,6 +68,58 @@ struct data
         vdata = nullptr;
         next = nullptr;
         previous = nullptr;
+        vdata_as_string = nullptr;
+    }
+
+    void yaml_get_data_as_string()
+    {
+        if(vdata_as_string)
+        {
+            memset(vdata_as_string, 0, strlen((cpint8) vdata_as_string));
+            free(vdata_as_string);
+            vdata_as_string = nullptr;
+        }
+        vdata_as_string = (puint8) calloc(1, sizeof(*vdata_as_string));
+        uint8 index = 0;
+        uint8 DAS_index = 0;
+
+        while(vdata[index])
+        {
+            vdata_as_string[DAS_index] = vdata[index] & 0xFF;
+            DAS_index++;
+            vdata_as_string = (puint8) realloc(
+                vdata_as_string,
+                (DAS_index + 1) * sizeof(*vdata_as_string)
+            );
+            
+            vdata_as_string[DAS_index] = (vdata[index] >> 8) & 0xFF;
+            DAS_index++;
+            vdata_as_string = (puint8) realloc(
+                vdata_as_string,
+                (DAS_index + 1) * sizeof(*vdata_as_string)
+            );
+            index++;
+        }
+    }
+
+    inline uint8 yaml_get_data_as_byte()
+    {
+        return vdata[0] & 0xFF;
+    }
+
+    inline bool yod_test(cpint8 if_is, bool must_be_string)
+    {
+        if(strcmp((cpint8) user_defined, if_is) == 0)
+        {
+            if(must_be_string)
+            {
+                yaml_get_data_as_string();
+                return true;
+            }
+            yaml_get_data_as_byte();
+            return true;
+        }
+        return false;
     }
 
     template<typename T>
@@ -70,6 +138,9 @@ struct data
 
         if(vdata) delete vdata;
         vdata = nullptr;
+
+        if(vdata_as_string) free(vdata_as_string);
+        vdata_as_string = nullptr;
     }
 };
 
@@ -94,10 +165,46 @@ public:
         struct data previous = *yaml_file_data;
         yaml_file_data->next = new struct data;
 
+        yaml_file_data->user_defined = new uint8[strlen((cpint8) user_def_name)];
         memcpy(yaml_file_data->user_defined, user_def_name, strlen((cpint8) user_def_name));
 
-        //yaml_file_data->data_type = type;
-        
+        yaml_file_data->data_type = type;
+        switch(yaml_file_data->data_type)
+        {
+            case data_types::Chr: yaml_file_data->vdata = (puint16) calloc(1, sizeof(*yaml_file_data->vdata));break;
+            case data_types::Dec:
+            case data_types::Hex: yaml_file_data->vdata = (puint16) calloc(2, sizeof(*yaml_file_data->vdata));break;
+            case data_types::Str: {
+                yaml_file_data->vdata = (puint16) calloc(1, sizeof(*yaml_file_data->vdata));
+                uint8 index = 0;
+
+                while(data[index])
+                {
+                    yaml_file_data->vdata[index] = data[index];
+                    index++;
+
+                    if(data[index])
+                        yaml_file_data->vdata = (puint16) realloc(
+                            yaml_file_data->vdata,
+                            (index + 1) * sizeof(*yaml_file_data->vdata)
+                        );
+                }
+                goto rest;
+                break;
+            }
+            default: break;
+        }
+        yaml_file_data->vdata = data;
+
+        rest:
+        /* Move on. */
+        yaml_file_data = yaml_file_data->next;
+        yaml_file_data->next = nullptr;
+
+        /* Previous data. */
+        yaml_file_data->previous = new struct data;
+        memcpy(yaml_file_data->previous, &previous, sizeof(*yaml_file_data->previous));
+        yaml_file_data_size++;
     }
 
     template<typename T>
